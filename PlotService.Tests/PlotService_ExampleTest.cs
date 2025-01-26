be5b9a770acb016c440837d129cae10bea4af0c7
Matt <19471665+mdloucks@users.noreply.github.com>
2025-01-26 15:39:17 -0500

    second dummy case

--- PlotService.Tests/PlotService_ExampleTest.cs
+++ PlotService.Tests/PlotService_ExampleTest.cs
@@ -14,4 +14,16 @@ namespace Prime.UnitTests.Services
             Assert.False(result, "1 should not be prime");
         }
     }
+
+    public class PrimeService_IsPrimeShouldInverted
+    {
+        [Fact]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var primeService = new PrimeService();
+            bool result = primeService.IsPrime(1);
+
+            Assert.True(!result, "1 should not be prime");
+        }
+    }
 }
